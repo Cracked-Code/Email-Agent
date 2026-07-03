8b57356eb30da51769a7194d29604912933e74d0
Nick <nickalot03@gmail.com>
2026-07-03 00:08:09 -0400

    1.1

--- nodes/draft_reply.py
+++ nodes/draft_reply.py
@@ -10,7 +10,10 @@ def draft_reply(state:EmailWriter) :
     llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
     prompt = f"""You are an email assistant writing on behalf of {state['account']}. Write a reply to this email. 
     From: {email['sender']}, Subject: {email['subject']}, Body: {email['body']}. If you refer in anyway the user, refer to them by {name}.
-    When signing off the email, use {name}"""
+    When signing off the email, use only {name}. A example of signing off is :
+    1. Best, Nick.
+    2. Thanks, Nick
+    3. Apperciate it, Nick"""
     if feedback:
         prompt += f" Previous draft was rejected. Feedback: {feedback}"
     
