7350fe24e3da5859c0d9710cbb22db7984dc1a9f
Nick <nickalot03@gmail.com>
2026-07-03 00:04:11 -0400

    Made the llm refer to the user better

--- nodes/draft_reply.py
+++ nodes/draft_reply.py
@@ -5,9 +5,12 @@ from state import EmailWriter
 def draft_reply(state:EmailWriter) :
     email = state['current_email']
     feedback = state['feedback']
+    name = state['name']
 
     llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
-    prompt = f"You are an email assistant writing on behalf of {state['account']}. Write a reply to this email. From: {email['sender']}, Subject: {email['subject']}, Body: {email['body']}. Sign off with: {state['name']}"
+    prompt = f"""You are an email assistant writing on behalf of {state['account']}. Write a reply to this email. 
+    From: {email['sender']}, Subject: {email['subject']}, Body: {email['body']}. If you refer in anyway the user, refer to them by {name}.
+    When signing off the email, use {name}"""
     if feedback:
         prompt += f" Previous draft was rejected. Feedback: {feedback}"
     
